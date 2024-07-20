9e7019e8b7e1a198547f7ea2e2d71592abb6124b
ajith <ajith9790217998@gmail.com>
2024-07-20 13:08:52 +0530

    Add User setup

--- CalibrationLab/Controllers/UserController.cs
+++ CalibrationLab/Controllers/UserController.cs
@@ -1,13 +1,14 @@
 namespace CalibrationLab.Controllers
 {
-
+    using CalibrationLab.Models;
     using Microsoft.AspNetCore.Mvc;
-
+    using MySql.Data.MySqlClient;
 
     public class UserController(ILogger<UserController> logger) : Controller
     {
 
         private readonly ILogger<UserController> _logger = logger;
+        private string _connectionString = "server=your_server;user=your_user;password=your_password;database=your_database;";
 
 
         public IActionResult Add()
@@ -15,6 +16,43 @@ namespace CalibrationLab.Controllers
             return View();
         }
 
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(User user, IFormFile Signature)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Signature != null)
+                {
+                    using var memoryStream = new MemoryStream();
+                    await Signature.CopyToAsync(memoryStream);
+                    user.Signature = memoryStream.ToArray();
+                }
+
+                using (var connection = new MySqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    using (var command = new MySqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandText = @"
+                    INSERT INTO users (Name, EmployeeId, Mail, Password, Signature)
+                    VALUES (@Name, @EmployeeId, @Mail, @Password, @Signature)";
+
+                        command.Parameters.AddWithValue("@Name", user.Name);
+                        command.Parameters.AddWithValue("@EmployeeId", user.EmployeeId);
+                        command.Parameters.AddWithValue("@Mail", user.Mail);
+                        command.Parameters.AddWithValue("@Password", user.Password);
+                        command.Parameters.AddWithValue("@Signature", user.Signature);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
 
         public IActionResult Edit()
         {
---
+++ CalibrationLab/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CalibrationLab.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string EmployeeId { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public byte[] Signature { get; set; }
+    }
+}
